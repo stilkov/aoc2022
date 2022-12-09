9e61e87a6236e79a04a71e3ce9aa3c636c926a38
Stefan Tilkov <stefan.tilkov@innoq.com>
2022-12-09 17:59:31 +0100

    Day 8 part 1

--- clj/src/aoc2022/core.clj
+++ clj/src/aoc2022/core.clj
@@ -3,7 +3,8 @@
 
 (require '[aoc2022.day1 :as day1]
          '[aoc2022.day2 :as day2]
-         '[aoc2022.day3 :as day3])
+         '[aoc2022.day3 :as day3]
+         '[aoc2022.day8 :as day8])
 
 
 (defn -main
@@ -14,4 +15,6 @@
   (println "Day 2 (1st half)" (day2/part1))
   (println "Day 2 (2nd half)" (day2/part2))
   (println "Day 3 (1st half)" (day3/part1))
-  (println "Day 3 (2nd half)" (day3/part2)))
+  (println "Day 3 (2nd half)" (day3/part2))
+  (println "Day 8 (1st half)" (day8/part1))
+)
---
+++ clj/src/aoc2022/day8.clj
@@ -0,0 +1,49 @@
+(ns aoc2022.day8)
+
+(require '[clojure.java.io :as io])
+
+(defn element-at [m a b]
+  ((m b) a))
+
+(defn width [m]
+  (count (m 0)))
+
+(defn height [m]
+  (count m))
+
+(defn top [m x y]
+  (map #(element-at m x %) (range 0 y)))
+
+(defn left [m x y]
+  (map #(element-at m % y) (range 0 x)))
+
+(defn right [m x y]
+  (map #(element-at m % y) (range (inc x) (width m))))
+
+(defn bottom [m x y]
+  (map #(element-at m x %) (range (inc y) (height m))))
+
+(defn visible-through [tree trees]
+  (every? #(< % tree) trees))
+
+(defn visible [m x y]
+  (let [e (element-at m x y)]
+    (or (visible-through e (right m x y))
+        (visible-through e (left m x y))
+        (visible-through e (top m x y))
+        (visible-through e (bottom m x y)))))
+
+(defn visible-trees [m]
+  (let [edges (+ (* 2 (width m)) (* 2 (height m)) -4)]
+    (+ edges (count (filter identity
+                            (for [x (range 1 (dec (width m)))
+                                  y (range 1 (dec (height m)))]
+                              (visible m x y)))))))
+
+(defn parse-char [c] (- (int c) (int \0)))
+(defn parse-line [line] (apply vector (map parse-char (seq line))))
+
+(defn part1 []
+  (let [lines (line-seq (io/reader "../input/day8.txt"))
+        matrix (apply vector (map parse-line lines))]
+    (visible-trees matrix)))
